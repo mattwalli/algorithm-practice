109c46c091c3d00fb618a8fb73b114444cd96246
mattwalli <mattwalli@gmail.com>
2020-06-04 20:14:19 -0700

    Time built in list sort

---
+++ timed_execution.py
@@ -0,0 +1,14 @@
+import random
+
+rand_nums = list()
+for n in range(1000000):
+    rand_nums.append(random.randint(-1000000, 1000000))
+
+def test_function():
+    rand_nums.sort()
+
+import timeit
+exec_time = timeit.timeit('test_function()', number = 1, globals=globals())
+print(f"Execution time: {exec_time} seconds")
+
+# built in sort: ~0.34 sec
