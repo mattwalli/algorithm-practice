72f828b888dedf610ecf1ad7d9589c6c1aa292b5
mattwalli <mattwalli@gmail.com>
2020-06-06 11:39:39 -0700

    weighted quick union my try

--- union_find.py
+++ union_find.py
@@ -2,20 +2,28 @@ class UF:
     def __init__(self, n):
         self.cnt = n
         self.id = list()
+        self.sz = list()
         for i in range(n):
             self.id.append(i)
+            self.sz.append(1)
 
     def union(self, p, q):
-        p_id = self.find(p)
-        q_id = self.find(q)
-        if p_id == q_id:
+        p_root = self.find(p)
+        q_root = self.find(q)
+        if p_root == q_root:
             return
-        self.id[p_id] = q_id
+        if self.sz[p_root] < self.sz[p_root]:
+            self.id[p_root] = q_root
+        else:
+            self.id[q_root] = p_root
         self.cnt -= 1
 
     def find(self, p):
+        new_size = 1
         while p != self.id[p]:
             p = self.id[p]
+            new_size += 1
+        self.sz[p] = new_size
         return p
 
     def connected(self, p, q):
@@ -25,8 +33,8 @@ class UF:
         return self.cnt
 
 #f = open("tinyUF.txt")
-f = open("mediumUF.txt")
-#f = open("largeUF.txt")
+#f = open("mediumUF.txt")
+f = open("largeUF.txt")
 uf = UF(int(f.readline()))
 while True:
     ln = f.readline()
@@ -35,8 +43,8 @@ while True:
     conn = ln.split()
     p = int(conn[0])
     q = int(conn[1])
-    if uf.connected(p, q):
-        continue
+    #if uf.connected(p, q):
+    #    continue
     uf.union(p, q)
     #print(f"{p} {q}")
 print(f"{uf.count()} components")
