337daac55597df154aca6bc73cf53e252a873adb
mattwalli <mattwalli@gmail.com>
2020-06-11 14:34:35 -0700

    Merge sort array top down

--- sort.py
+++ sort.py
@@ -22,6 +22,35 @@ def insertion_sort_2(data):
             j -= 1
         data[j] = temp
 
+def merge_sort(data):
+
+    def merge(data, low, mid, high):
+        copy = data.copy()
+        i, j = low, mid+1
+        for k in range(low, high+1):
+            if i > mid:
+                data[k] = copy[j]
+                j += 1
+            elif j > high:
+                data[k] = copy[i]
+                i += 1
+            elif copy[i] < copy[j]:
+                data[k] = copy[i]
+                i += 1
+            else:
+                data[k] = copy[j]
+                j += 1
+    
+    def sort(data, low, high):
+        if (high - low) <= 0:
+            return
+        mid = low + (high - low) // 2
+        sort(data, low, mid)
+        sort(data, mid+1, high)
+        merge(data, low, mid, high)
+
+    sort(data, 0, len(data) - 1)
+
 def is_sorted(data):
     for i in range(1, len(data)):
         if data[i] < data[i-1]:
@@ -31,9 +60,9 @@ def is_sorted(data):
 from timeit import timeit
 from random import randint
 data = list()
-for n in range(20000):
+for n in range(40000):
     data.append(randint(0, 1000000))
 
-exec_time = timeit('insertion_sort_2(data)', number=1, globals=globals())
+exec_time = timeit('merge_sort(data)', number=1, globals=globals())
 print(f"Execution time: {exec_time} seconds")
 print(f"Is sorted: {is_sorted(data)}")
